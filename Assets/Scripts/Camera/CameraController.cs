904988662d030c0887124aa1a1d824c86cb75aee
AlexTallen <AlexTallen@PMS>
2018-04-26 12:05:51 +0200

    Update
    Hopp?

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -10,7 +10,7 @@ public class CameraController : MonoBehaviour
     public Vector3 offset;
 
 	// Update is called once per frame
-	void FixedUpdate ()
+	void Update ()
     {
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
