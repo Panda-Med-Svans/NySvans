365ca43a78b88fc85c1da13bc5e0d98630a00ae1
AlexTallen <AlexTallen@PMS>
2018-05-03 01:30:41 +0200

    Highscore ändringar
    En ui bug, om man kollar runt i uit efter man startat spelet så försvinner uit. lätt fixat, ska bara hitta vart det händer

--- Assets/Scripts/Mechanics/ScoreTracker.cs
+++ Assets/Scripts/Mechanics/ScoreTracker.cs
@@ -5,18 +5,68 @@ using UnityEngine.UI;
 
 public class ScoreTracker : MonoBehaviour {
 
-    public int leafCount;
-    public Text leafText;
+    private GameObject startHighscore;
+    private GameObject pauseScreen;
+    private GameObject pauseHigh;
+
+    private GameObject startLeafTarget;
+    private Text startLeafText;
+    private GameObject pauseLeafTarget;
+    private Text pauseLeafText;
+
+    private GameObject startOwlTarget;
+    private Text startOwlText;
+    private GameObject pauseOwlTarget;
+    private Text pauseOwlText;
+
+    
+    public string startLeaf;
+    public string pauseleaf;
+    public string startOwl;
+    public string pauseOwl;
     [Space]
+    public int leafCount;
     public int owlCount;
-    public Text owlText;
 
     void Start()
     {
-        leafText.text = PlayerPrefs.GetInt("LeafScore", 0).ToString(leafCount + " Leafs out of 27 Leafs");
-        owlText.text = PlayerPrefs.GetInt("OwlScore", 0).ToString(owlCount + " Owls out of Six Owls");
-    }
+        #region Finding Score Targets
 
+        //Hittar parents till menyerna för att senare SetActive(false)
+        startHighscore = GameObject.Find("StartHighscore");
+        pauseScreen = GameObject.Find("Pause");
+        pauseHigh = GameObject.Find("PauseHighscore");
+
+        //Hittar Löv Highscore texten för start Skärms Highscore menyn
+        startLeafTarget = GameObject.Find(startLeaf);
+        startLeafText = startLeafTarget.GetComponentInChildren<Text>();
+
+        //Hittar Löv Highscore texten för start Paus Highscore menyn
+        pauseLeafTarget = GameObject.Find(pauseleaf);
+        pauseLeafText = pauseLeafTarget.GetComponentInChildren<Text>();
+
+        //Hittar Ugglor Highscore texten för start Paus Highscore menyn
+        startOwlTarget = GameObject.Find(startOwl);
+        startOwlText = startOwlTarget.GetComponentInChildren<Text>();
+
+        //Hittar Ugglor Highscore texten för start Paus Highscore menyn
+        pauseOwlTarget = GameObject.Find(pauseOwl);
+        pauseOwlText = pauseOwlTarget.GetComponentInChildren<Text>();
+
+        //Avaktiverar menyerna tills de ska användas igen
+        startHighscore.SetActive(false);
+        pauseScreen.SetActive(false);
+        pauseHigh.SetActive(false);
+        #endregion
+
+        startLeafText.text = pauseLeafText.text = PlayerPrefs.GetInt("LeafScore", 0).ToString("Leafs: " + leafCount + " / 27 Leafs");
+        startOwlText.text = pauseOwlText.text = PlayerPrefs.GetInt("OwlScore", 0).ToString("Owls: " + owlCount + " / 6 Owls");
+
+
+        //startHighscore.SetActive(false);
+        //pauseScreen.SetActive(false);
+    }
+    //lägg till en till pub text var för pausehighscore
     void Update ()
     {
 
@@ -27,7 +77,7 @@ public class ScoreTracker : MonoBehaviour {
         if (owlCount > PlayerPrefs.GetInt("OwlScore", 0))
         {
             PlayerPrefs.SetInt("OwlScore", owlCount);
-            owlText.text = owlCount.ToString(owlCount + " Owls out of Six Owls");
+            startOwlText.text = pauseOwlText.text = owlCount.ToString("Owls: " + owlCount + " / 6 Owls");
         }
         //Ändra highscore för ugglorna
     }
@@ -37,7 +87,7 @@ public class ScoreTracker : MonoBehaviour {
         if(leafCount > PlayerPrefs.GetInt("LeafScore", 0))
         {
             PlayerPrefs.SetInt("LefScore", leafCount);
-            leafText.text = leafCount.ToString(leafCount  + " Leafs out of 27 Leafs");
+            startLeafText.text = pauseLeafText.text = leafCount.ToString("Leafs: " + leafCount + " / 27 Leafs");
         }
         //Ändra highscore för löven
     }
@@ -48,10 +98,10 @@ public class ScoreTracker : MonoBehaviour {
     public void ResetScore()
     {
         PlayerPrefs.DeleteKey("LeafScore");
-        leafText.text = "0";
+        startLeafText.text = pauseLeafText.text = "Leafs: 0 / 27 Leafs";
         leafCount = 0;
         PlayerPrefs.DeleteKey("OwlScore");
-        owlText.text = "0";
+        startOwlText.text = pauseOwlText.text = "Owls: 0 / 6 Owls";
         owlCount = 0;
     }
 }
